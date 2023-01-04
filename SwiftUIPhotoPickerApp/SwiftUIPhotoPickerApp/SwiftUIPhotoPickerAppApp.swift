5f0b9619161cd0fdf28b86439bd332139935b524
Gurjinder Singh <gurjindersingh663@gmail.com>
2023-01-04 19:07:12 +0530

    initial commit project created

---
+++ SwiftUIPhotoPickerApp/SwiftUIPhotoPickerApp/SwiftUIPhotoPickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoPickerAppApp.swift
+//  SwiftUIPhotoPickerApp
+//
+//  Created by Gurjinder Singh on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoPickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

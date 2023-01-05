82506a1754a0040e302b4440736e660879dce126
Gurjinder Singh <gurjindersingh663@gmail.com>
2023-01-05 22:53:17 +0530

    multiple images func added

--- SwiftUIPhotoPickerApp/SwiftUIPhotoPickerApp/ContentView.swift
+++ SwiftUIPhotoPickerApp/SwiftUIPhotoPickerApp/ContentView.swift
@@ -10,36 +10,49 @@ import PhotosUI
 
 struct ContentView: View {
     
-    @State private var selectedItem: PhotosPickerItem? = nil
-    @State private var selectedImageData: Data? = nil
-
+    //MARK: - Properties
+    
+    @State private var selectedItem: [PhotosPickerItem] = [PhotosPickerItem]()
+    @State private var selectedImageData: [Data]? = [Data]()
+    
+    //MARK: - Body
+    
     var body: some View {
         VStack {
+
                 PhotosPicker(
                     selection: $selectedItem,
+                    maxSelectionCount: 2,
                     matching: .images,
                     photoLibrary: .shared()
+                    
                 ) {
-                    Text("Choose Photo")
-                        .font(.title3)
-                        .fontWeight(.bold)
+                    Text("Choose Photos from Gallery")
                         .frame(width: 350, height: 50)
                         .background(Capsule().stroke(lineWidth: 2))
                 }
                 .onChange(of: selectedItem) { newValue in
                     Task {
-                        if let data = try? await newValue?.loadTransferable(type: Data.self) {
-                            selectedImageData = data
+                        if let data = try? await newValue.first!.loadTransferable(type: Data.self) {
+                            selectedImageData?.append(data)
+                        }
+                        if let data = try? await newValue.last!.loadTransferable(type: Data.self) {
+                            selectedImageData?.append(data)
                         }
                     }
                 }
-            if let selectedImageData, let uiImage = UIImage(data: selectedImageData) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
+            
+            ForEach(selectedImageData!,id: \.self) { item in
+                if let uiImage = UIImage(data: (item)) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16).stroke(Color.yellow, lineWidth: 8)
+                        )
+                }
             }
-
         }
         .padding()
     }
